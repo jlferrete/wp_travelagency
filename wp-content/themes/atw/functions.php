0b0f5b3f7efb6b44cbb81abd9185359a3019f50e
JlFerrete <joseluisferrete@gmail.com>
2019-11-25 21:31:55 +0100

    Tours CPT

--- wp-content/themes/atw/functions.php
+++ wp-content/themes/atw/functions.php
@@ -398,35 +398,33 @@ function create_post_type_html5()
 {
     register_taxonomy_for_object_type('category', 'html5-blank'); // Register Taxonomies for Category
     register_taxonomy_for_object_type('post_tag', 'html5-blank');
-    register_post_type('html5-blank', // Register Custom Post Type
+    register_post_type('tours', // Register Custom Post Type
         array(
         'labels' => array(
-            'name' => __('HTML5 Blank Custom Post', 'html5blank'), // Rename these to suit
-            'singular_name' => __('HTML5 Blank Custom Post', 'html5blank'),
+            'name' => __('Tours', 'html5blank'), // Rename these to suit
+            'singular_name' => __('Tours', 'html5blank'),
             'add_new' => __('Add New', 'html5blank'),
-            'add_new_item' => __('Add New HTML5 Blank Custom Post', 'html5blank'),
+            'add_new_item' => __('Add New Tours', 'html5blank'),
             'edit' => __('Edit', 'html5blank'),
-            'edit_item' => __('Edit HTML5 Blank Custom Post', 'html5blank'),
-            'new_item' => __('New HTML5 Blank Custom Post', 'html5blank'),
-            'view' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'view_item' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'search_items' => __('Search HTML5 Blank Custom Post', 'html5blank'),
-            'not_found' => __('No HTML5 Blank Custom Posts found', 'html5blank'),
-            'not_found_in_trash' => __('No HTML5 Blank Custom Posts found in Trash', 'html5blank')
+            'edit_item' => __('Edit Tours', 'html5blank'),
+            'new_item' => __('New Tours', 'html5blank'),
+            'view' => __('View Tours', 'html5blank'),
+            'view_item' => __('View Tours', 'html5blank'),
+            'search_items' => __('Search Tours', 'html5blank'),
+            'not_found' => __('No se encontraron tours', 'html5blank'),
+            'not_found_in_trash' => __('No Tours found in Trash', 'html5blank')
         ),
         'public' => true,
         'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
         'has_archive' => true,
+        'menu_position' => 6,
         'supports' => array(
             'title',
             'editor',
-            'excerpt',
             'thumbnail'
         ), // Go to Dashboard Custom HTML5 Blank post for supports
         'can_export' => true, // Allows export in Tools > Export
         'taxonomies' => array(
-            'post_tag',
-            'category'
         ) // Add Category and Post Tags support
     ));
 }
